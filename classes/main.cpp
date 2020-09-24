6e1f5054db95b33431395fdeb33abfa54f4935d8
AnmolKumar Shah <anmolshah.20k@gmail.com>
2020-09-24 20:51:03 +0530

    operator overloading and friend function and scope resolution

--- classes/main.cpp
+++ classes/main.cpp
@@ -14,21 +14,29 @@ public:
         this->real = c.real;
         this->img = c.img;
     }
-    Complex add(Complex c){
-        Complex temp;
-        temp.real = this->real + c.real;
-        temp.img = this->img + c.img;
-        return temp;
-    }
+    Complex operator+(Complex c);
     void display(){
         cout<<real<<" + i"<<img<<endl;
     }
 
+    friend ostream & operator<<(ostream &o, Complex &c);
 };
+
+ostream & operator<<(ostream &o, Complex &c){
+    o<<c.real<<" + i"<<c.img<<endl;
+    return o;
+}
+
+Complex Complex::operator+(Complex c){
+    Complex temp;
+    temp.real = this->real + c.real;
+    temp.img = this->img + c.img;
+    return temp;
+}
 
 int main(){
     Complex c1(3,4),c2(4,6),c3;
-    c3 = c1.add(c2);
-    c3.display();
+    c3 = c1+c2;
+    cout<<c3;
 return 0;
 }
